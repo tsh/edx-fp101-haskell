f1d17b6fe1fc8d9112cb04bde2da709dd3d42bc4
Anton Nikulin <dr.tallin@gmail.com>
2015-10-17 21:16:13 +0300

    move files to 'old'

---
+++ old/lab3.hs
@@ -0,0 +1,41 @@
+module Lab3 where
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- LIST COMPREHENSIONS
+------------------------------------------------------------------------------------------------------------------------------
+
+-- ===================================
+-- Ex. 0 - 2
+-- ===================================
+
+evens :: [Integer] -> [Integer]
+evens xs = [x| x <- xs, even x]
+
+-- ===================================
+-- Ex. 3 - 4 
+-- ===================================
+
+-- complete the following line with the correct type signature for this function
+squares :: Integer -> [Integer]
+squares n = [x*x | x <- [1..n]]
+
+sumSquares :: Integer -> Integer
+sumSquares n = sum (squares n)
+
+-- ===================================
+-- Ex. 5 - 7
+-- ===================================
+
+-- complete the following line with the correct type signature for this function
+-- squares' :: ...
+squares' m n = [x*x| (x,i) <- zip [n+1..] [0..m], i <m]
+
+sumSquares' :: Integer -> Integer
+sumSquares' x = sum . uncurry squares' $ (x, x)
+
+-- ===================================
+-- Ex. 8
+-- ===================================
+
+coords :: Integer -> Integer -> [(Integer,Integer)]
+coords x y = [(x,y) | x <- [0..x], y <-[0..y]]
